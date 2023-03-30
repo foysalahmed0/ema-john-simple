ea6b65a9d18aee7f8789c3281d8453cfe0f3a28d
mdfoysalahmed0 <mdfoysalahmed.info@gmail.com>
2023-03-30 20:56:54 +0600

    added final

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -14,9 +14,30 @@ const Shop = () => {
         .then(data =>setProducts(data))
     }, []);
 
+    useEffect( ()=>{
+        const storedCard = getShoppingCart();
+        const savedCart = [];
+        // step 1 : get id
+        for(const id in storedCard){
+        // step 2: get the product by using id
+            const addedProduct = products.find(product => product.id === id)
+            if(addedProduct){
+            // step 3: get quantity of the product
+            const quantity = storedCard[id];
+            addedProduct.quantity = quantity;
+            // step 4: add thr added product to the save cart
+            savedCart.push(addedProduct);
+            }
+            // console.log(addedProduct);
+        }
+        // step 5: set the cart
+        setCart(savedCart);
+    } , [products])
+
     const handelAddToCart = (product) =>{
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
     return (
         <div className='shop-container'>
